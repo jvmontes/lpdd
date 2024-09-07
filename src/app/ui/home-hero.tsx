befdd0190e73aa57f0031f14431a79bb319990e1
Jorge Viramontes <>
2024-09-07 18:27:48 -0500

    feat: change font sizes for mobile on hero.

--- src/app/ui/home-hero.tsx
+++ src/app/ui/home-hero.tsx
@@ -16,11 +16,11 @@ export default function HomeHero() {
     <section className="relative w-full h-[50vh] md:h-[75vh] bg-slate-900">
       <div className="absolute bottom-[10%] md:bottom-[20%] left-[5%]">
         <h1
-          className={`${lexendMedium.className} text-white text-2xl md:text-3xl pb-3`}
+          className={`${lexendMedium.className} text-white text-base md:text-3xl pb-1 md:pb-3`}
         >
           Latiné Professional Development Directory
         </h1>
-        <h2 className={`${koulen.className} text-white text-6xl md:text-8xl`}>
+        <h2 className={`${koulen.className} text-white text-xl md:text-8xl`}>
           Find Your Community
         </h2>
       </div>
