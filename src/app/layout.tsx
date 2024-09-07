146f4e3e3c752fe481e706ef2b0c81febb8b7f4d
Jorge Viramontes <>
2024-09-07 17:12:24 -0500

    refactor: update font to match designs.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Lexend } from "next/font/google";
 import "./globals.css";
 import NavBar from "./ui/nav-bar";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Lexend({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Latiné Professional Development Directory",
