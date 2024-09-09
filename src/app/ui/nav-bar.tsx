54b80ecc5a09b01f34ccbb77e499075c6886a7e5
Jorge Viramontes <>
2024-09-09 09:37:00 -0500

    refactor: simplify Link & clarify active link logic.

--- src/app/ui/nav-bar.tsx
+++ src/app/ui/nav-bar.tsx
@@ -4,7 +4,7 @@ import NavLinks from "./nav-links";
 export default function NavBar() {
   return (
     <div className="w-full justify-between flex h-8 text-blue-800">
-      <Link key={"Home"} href={"/"}>
+      <Link href="/">
         <p className="px-4">Home</p>
       </Link>
       <NavLinks />
