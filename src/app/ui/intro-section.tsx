75de33ae23b837576affa98274b988cc122038a7
Gerardo Rodriguez <gerarodriguez@proton.me>
2024-09-20 15:31:46 -0500

    feat: (mobile) looks good to go

--- src/app/ui/intro-section.tsx
+++ src/app/ui/intro-section.tsx
@@ -16,13 +16,14 @@ const lexendSemi = Lexend({
 
 export default function IntroSection() {
   return (
-    <section className="border border-black w-full py-8 text-center">
+    <section className="w-full py-8 text-center">
 
-        <p className={`px-8 text-[#717171] text-xs ${lexendMedium.className}`}>We strive to provide a comprehensive and updated list of organizations that are dedicated to</p>
+        <p className={`mx-6 text-[#717171] text-xs ${lexendMedium.className}`}>We strive to provide a comprehensive and updated list of organizations that are dedicated to</p>
         
-        <h2 className={`my-3 px-1 text-lg ${lexendSemi.className}`}>Fostering The Growth And Development Of Latiné Professionals</h2>
+        <h2 className={`mt-4 mb-5 mx-6 text-lg ${lexendSemi.className}`}>Fostering The Growth And Development Of Latiné Professionals</h2>
+        {/* <p className={`my-3 text-lg ${lexendSemi.className}`}>Fostering The Growth And Development Of Latiné Professionals</p> */}
         
-        <Link href="/about" className="">About</Link>
+        <Link href="/about" className={`px-5 py-1 border-2 rounded-full border-[#AF8D86] text-[#AF8D86] text-sm ${lexendRegular.className}`}>About</Link>
     </section>
   );
 }
