6354b293fb2eef2fd45a898855b023e71fe3a4fa
Gerardo Rodriguez <gerarodriguez@proton.me>
2024-09-21 12:29:53 -0500

    chore: deleted old code that was commented out

--- src/app/ui/nav-bar.tsx
+++ src/app/ui/nav-bar.tsx
@@ -10,26 +10,11 @@ export default function NavBar() {
   const pathname = usePathname();
 
   const navClasses = clsx(
-    // "w-full h-14 flex flex-col justify-center bg-slate-900 absolute z-10",
     "w-full h-14 flex items-center justify-between px-2 bg-slate-900",
     pathname === '/' && "absolute z-10 sm:bg-transparent"
   );
 
   return (
-    // <nav className={navClasses}>
-    //   <div className="flex justify-between px-2">
-    //     <Link href="/" className="flex items-center mr-12">
-    //       <Image
-    //         src="/logo.svg"
-    //         width={40}
-    //         height={40}
-    //         alt="Logo for Latiné Professional Development Directory"
-    //       />
-    //       <p className="text-xl text-white tracking-tighter">LPDD</p>
-    //     </Link>
-    //     <NavLinks />
-    //   </div>
-    // </nav>
     <nav className={navClasses}>
         <Link href="/" className="flex items-center mr-12">
           <Image
