06a3eaf2d4346848811c61e66b42f3cc05082da3
Gerardo Rodriguez <gerarodriguez@proton.me>
2024-09-21 11:43:14 -0500

    style: adjusted breakpoint for flex-col on headers div

--- src/app/ui/home-hero.tsx
+++ src/app/ui/home-hero.tsx
@@ -15,18 +15,21 @@ const lexendMedium = Lexend({
 export default function HomeHero() {
   return (
     <section className="relative w-full h-[30vh] md:h-[75vh] bg-black">
+
+      {/* img div */}
       <div className="w-full h-full">
         <Image
           src="/hero.jpg"
           priority={true}
           width={2048}
           height={1365}
-          className="w-full h-full object-cover sm:object-[0%_80%] opacity-40"
+          className="w-full h-full object-cover md:object-[0%_70%] opacity-40"
           alt="Group of people listening to a panel discussion with panelists seated on stage."
         />
       </div>
 
-      <div className="absolute bottom-[20%] left-[5%] flex flex-col-reverse sm:flex-col">
+      {/* headers div */}
+      <div className="absolute bottom-[20%] left-[5%] flex flex-col-reverse md:flex-col">
         <h1 className={`${lexendMedium.className} text-white text-xs md:text-3xl`}>
           Latiné Professional Development Directory
         </h1>
@@ -34,13 +37,7 @@ export default function HomeHero() {
           Find Your Community
         </h2>
       </div>
-
-      {/* <div>
-        <div className="flex flex-col sm:flex-col-reverse">
-          <h1 className="text-2xl">This is H1</h1>
-          <h2 className="text-xl">This is H2</h2>
-        </div>
-      </div> */}
+      
     </section>
   );
 }
