e6ff12ea5a028d15bd719968e58cdff8f82632c0
Jorge Viramontes <>
2024-09-23 11:07:06 -0500

    refactor: remove comment and revert baseClasses const to enable IntelliSense tooltip. Also add a subtle transition for the scale shift.

--- src/app/ui/nav-links.tsx
+++ src/app/ui/nav-links.tsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import clsx from "clsx";
 
-// home - about - contact - login
 const links = [
   {
     name: "HOME",
@@ -24,26 +23,23 @@ const links = [
   // },
 ];
 
-
 export default function NavLinks() {
   const pathname = usePathname();
-  
-  const baseClasses = "text-[0.70rem] pr-2 tracking-tighter sm:text-sm sm:pl-8 sm:tracking-normal";
+
   const activeClasses = "text-blue-300 underline scale-95";
   const inactiveClasses = "text-white";
-  
+
   return (
     <div className="flex">
-      {links.map(({ name, href}) => {
+      {links.map(({ name, href }) => {
         const isActiveLink = pathname === href;
 
         return (
           <Link key={name} href={href}>
             <p
-              className={clsx(
-                baseClasses,
-                isActiveLink ? activeClasses : inactiveClasses
-              )}
+              className={`text-sm pr-2 tracking-tighter sm:pl-8 sm:tracking-normal
+                transition ease-in-out duration-300
+                ${clsx(isActiveLink ? activeClasses : inactiveClasses)}`}
             >
               {name}
             </p>
