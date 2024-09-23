f760d1f8ecc31f86c2e55fc326f804da2130ad30
Jorge Viramontes <>
2024-09-23 15:53:18 -0500

    feat: Update changes to what Gera implemented.

--- src/app/ui/home-hero.tsx
+++ src/app/ui/home-hero.tsx
@@ -14,24 +14,24 @@ const lexendMedium = Lexend({
 
 export default function HomeHero() {
   return (
-    <section className="relative h-[50vh] w-full bg-black md:h-[75vh]">
+    <section className="relative h-[25vh] w-full bg-black md:h-[50vh] lg:h-[65vh]">
       <div className="h-full w-full">
         <Image
           src="/hero.jpg"
           priority={true}
           width={2048}
           height={1365}
-          className="h-full w-full object-cover object-[0%_80%] opacity-40"
+          className="h-full w-full object-cover opacity-40 lg:object-[30%_60%]"
           alt="Group of people listening to a panel discussion with panelists seated on stage."
         />
       </div>
-      <div className="absolute bottom-[10%] left-[5%] md:bottom-[20%]">
+      <div className="absolute bottom-[25%] left-[5%] flex flex-col-reverse sm:flex-col">
         <h1
-          className={`${lexendMedium.className} pb-3 text-2xl text-white md:text-3xl`}
+          className={`${lexendMedium.className} pb-3 text-xs text-white md:text-3xl`}
         >
           Latiné Professional Development Directory
         </h1>
-        <h2 className={`${koulen.className} text-xl text-white md:text-8xl`}>
+        <h2 className={`${koulen.className} text-4xl text-white md:text-9xl`}>
           Find Your Community
         </h2>
       </div>
