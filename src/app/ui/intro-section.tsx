f0f742f0eb6a7627f2f919a94fbf4dea6d80cba4
Jorge Viramontes <>
2024-09-23 14:12:43 -0500

    Run prettier on intro section, add .vscode/settings.json to format on save using prettier

--- src/app/ui/intro-section.tsx
+++ src/app/ui/intro-section.tsx
@@ -10,17 +10,16 @@ const lexend = Lexend({
 export default function IntroSection() {
     return (
         <section
-            className={`w-full pt-8 text-center sm:pt-20 flex flex-col items-center ${lexend.className}`}
+            className={`flex w-full flex-col items-center pt-8 text-center sm:pt-20 ${lexend.className}`}
         >
-            <p className="mx-6 text-xs font-medium sm:text-2xl
-            text-gray-600 dark:text-gray-400">
-                We strive to provide a comprehensive and updated list of organizations that are dedicated to
+            <p className="mx-6 text-xs font-medium text-gray-600 sm:text-2xl dark:text-gray-400">
+                We strive to provide a comprehensive and updated list of
+                organizations that are dedicated to
             </p>
             <h2 className="mx-4 mb-4 mt-2 text-lg font-semibold sm:mx-auto sm:mb-10 sm:mt-4 sm:max-w-3xl sm:text-4xl">
                 Fostering The Growth And Development Of Latiné Professionals
             </h2>
-            <Button href='/about' title='About'></Button>
-
+            <Button href="/about" title="About"></Button>
         </section>
     )
 }
